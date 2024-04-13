b2a608b0d84674cf2b4da5f00c2c408473f47a4d
hector994 <50502676+hector994@users.noreply.github.com>
2024-04-12 23:34:04 -0600

    Initial commit

---
+++ app/src/main/java/com/example/miagenda/Configuraciones.java
@@ -0,0 +1,9 @@
+package com.example.miagenda;
+
+public class Configuraciones {
+    String URLserver;
+
+    public Configuraciones() {
+        this.URLserver = "http://192.168.0.2:8080/app_server/webservice02.php";
+    }
+}
